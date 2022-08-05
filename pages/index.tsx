54d7a9006761a4ef944c3b8215ceae847eb3705e
Himanshu <himanshu.kumarr07@gmail.com>
2022-08-05 08:43:14 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import VideoCard from '../components/VideoCard';
+import axios from 'axios'
+import { Video } from '../types';
+import NoResults from '../components/NoResults';
+import { BASE_URL } from '../utils';
+
+interface IProps {
+  videos: Video []; 
+}
+
+
+const Home = ({videos} : IProps) => {
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+
+      {videos.length
+
+      ? videos?.map((video : Video) => (
+        <VideoCard post = {video} isShowingOnHome key = {video._id} />
+      ))
+      : <NoResults text = {'No Videos'} /> }
+    
+    </div>
+  )
+}
+
+
+export const getServerSideProps = async () => {
+  const {data} = await axios.get(`${BASE_URL}/api/post`);
+  return {
+    props: {
+      videos: data
+    }
+  }
+}
+
+export default Home
+
+
+
